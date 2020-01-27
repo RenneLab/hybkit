1c862ec189fd91591d1bb0c0f43b81e8cef25448
Dan Stribling <ds@ufl.edu>
2020-01-27 14:07:01 -0500

    Refactored plot prototype.

--- hybkit/plot.py
+++ hybkit/plot.py
@@ -12,60 +12,80 @@ from hybkit.__about__ import __author__, __contact__, __credits__, __date__, __d
                              __email__, __license__, __maintainer__, __status__, __version__
 
 import hybkit
+import matplotlib.pyplot as plot
 
-
+# Public Constants
+DEFAULT_PIE_MATPLOTLIB_SETTINGS = {
+    autopct='%1.1f%%',
+    shadow=False,
+    startangle=90
+    }
+DEFAULT_PIE_DPI = 600
+DEFAULT_PIE_FILE_TYPE = 'png'
+DEFAULT_PIE_OTHER_THRESHHOLD = 0.1
+DEFAULT_PIE_MIN_WEDGE_SIZE = 0.05
 
 # Public Methods : HybRecord Type Analysis Plotting
-def plot_type_analysis_hybrid_type_counts(analysis_dict, sep=','):
-    'Plot the results of hybrid_type_counts in a list of sep-delimited lines.'
-    # Sort by count in descending order
-    ret_lines = ['hybrid_type' + sep + 'count']
-    sorted_pairs = sorted(analysis_dict['hybrid_type_counts'].items(),
-                          key=lambda item: item[1], reverse=True)
-    other_threshhold = 0.90
-    total_count = 0
-    for key, count in sorted_pairs:
-        total_count += count
-    labels = []
-    counts = []
-    running_total = 0
-    for key, count in sorted_pairs:
-        running_total += count
-        if running_total/total_count > other_threshhold:
-            labels.append('Other')
-            counts.append(total_count - running_total)
-            break
-        else:
-            labels.append(key)
-            counts.append(count)
+def plot_hybrid_type_counts(analysis_dict, plot_file_name, 
+                    other_threshhold=DEFAULT_PIE_OTHER_THRESHHOLD,
+                    min_wedge_size=DEFAULT_PIE_MIN_WEDGE_SIZE,
+                    plot_file_type=DEFAULT_PIE_FILE_TYPE,
+                    dpi=DEFAULT_PIE_DPI,
+                    matplotlib_settings=DEFAULT_PIE_MATPLOTLIB_SETTINGS):
+    'Plot the results of hybrid_type_counts'
+    # Sort hybrid_type_counts counter object in descending order
+    for key, count in analysis_dict['hybrid_type_counts'].most_common() 
+        labels.append(key)
+        counts.append(count)
 
-    _plot_pie_chart(labels,counts)
+    _plot_pie_chart(label=labels,
+                    sizes=counts,
+                    plot_file_name=plot_file_name,
+                    other_threshhold=other_threshhold,
+                    min_wedge_size=min_wedge_size,
+                    plot_file_type=plot_file_type,
+                    dpi=dpi,
+                    matplotlib_settings=matplotlib_settings,
+                   )
 
-# Private Methods : Utility
-def _check_matplotlib():
-    # lazy-importing is being used to allow matplotlib to serve as an 
-    #   optional dependency for the package.
-    try:
-        import matplotlib
-    except ModuleNotFoundError:
-        message = 'The python package matplotlib is required for plotting funciton.'
-        message += '\nPlease install this package before using plotting features.'
-        print(message)
-        raise
 
 # Private Methods : Pie Chart
-def _plot_pie_chart(labels, sizes):
-    _check_matplotlib()
-    import matplotlib.pyplot as plot
-    # Data to plot
-    #explode = (0.1, 0, 0, 0)  # explode 1st slice
-    
-    # Plot
-    plot.pie(sizes, labels=labels, # explode=explode, colors=colors,
-             autopct='%1.1f%%', shadow=False, startangle=90)
-    
+def _plot_pie_chart(labels, sizes, plot_file_name,
+                    other_threshhold=DEFAULT_PIE_OTHER_THRESHHOLD
+                    min_wedge_size=DEFAULT_PIE_MIN_WEDGE_SIZE,
+                    plot_file_type=DEFAULT_PIE_FILE_TYPE,
+                    dpi=DEFAULT_PIE_DPI,
+                    matplotlib_settings=DEFAULT_PIE_MATPLOTLIB_SETTINGS):
+    total_size = sum(counts)
+    fraction_sizes = [size/total_size for size in sizes]
+    use_labels = []
+    use_sizes = []
+    for i in range(len(labels)):
+        total_fraction = sum(use_sizes) / total_size
+        if ((fraction_size[i] < min_wedge_size)
+            or (total_fraction > (1 - other_threshhold) and i != (len(labels) - 1)):
+            break
+        else:
+            use_labels.append(labels[i])
+            use_sizes.append(sizes[i])
+
+    if len(use_labels) != len(labels):
+        other_size = total_size - sum(sizes)
+        use_labels.append('other')
+        use_sizes.append(other_size)
+
+    # Reverse ordering to create expected clockwise big->small order.
+    use_labels_ordered = reversed(use_labels)
+    use_sizes_ordered = reversed(use_sizes)
+
+    plot.pie(use_sizes_ordered, 
+             labels=use_labels_ordered, 
+             **matplotlib_settings)
     plot.axis('equal')
-    plot.show()
+    if not plot_file_name.endswith(plot_file_type):
+        plot_file_name += '.' + plot_file_type
+    plot.savefig(plot_file_name)  
+
     #patches, texts = plt.pie(sizes, colors=colors, shadow=True, startangle=90)
     #plt.legend(patches, labels, loc="best")
     #plt.axis('equal')
