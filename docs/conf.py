cb821813ffa11097c4c44f6e2a78d582eca99c92
Dan Stribling <ds@ufl.edu>
2020-02-05 15:09:09 -0500

    Add google style support to docs.

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ release = hybkit.__about__.__version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
 # add_module_names
 autodoc_member_order = 'bysource'
